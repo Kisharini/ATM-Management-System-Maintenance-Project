79471e1753d0bf02f41045b8238ce26808fcc056
Kisharini Jeeva Nathan <224618@student.upm.edu.my>
2026-01-06 15:43:57 +0000

    update

--- atm/src/atm/AtmMainDriver.java
+++ atm/src/atm/AtmMainDriver.java
@@ -9,7 +9,7 @@ public class AtmMainDriver
     public static void main(String[] args) 
     {
         LoginForm f = new LoginForm ();        
-        f.setVisible(false);
+        f.setVisible(true);
         f.setSize(540,350);
         f.setLocation(450,220);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
