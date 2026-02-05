ba418d8e8df6259514aa002361211b0fa14948c3
ainnurqiss <ainnurbalqis110104@example.com>
2026-02-06 00:43:09 +0800

    Add CR-01 comment for daily withdrawal limit maintenance

--- atm/src/atm/AccountData.java
+++ atm/src/atm/AccountData.java
@@ -1,27 +1,28 @@
 package atm;
+
 import javax.swing.*;
 
-public class AccountData 
-{
- 	String pincode;
+public class AccountData {
+	String pincode;
 	String customername;
 	String accounttype;
 	String accountnumber;
 	String startbalance;
-	
-	public AccountData(String p,String c,String a,String an,String s)
-	{
-		pincode=p;
-		customername=c;
-		accounttype=a;
-		accountnumber=an;
-		startbalance=s;
+
+	// CR-01: Daily withdrawal limit validation added for maintenance updategit
+	public AccountData(String p, String c, String a, String an, String s) {
+		pincode = p;
+		customername = c;
+		accounttype = a;
+		accountnumber = an;
+		startbalance = s;
 	}
-	
-	public void print()
-	{
-		JOptionPane.showMessageDialog(null,"PINCODE : "+pincode+"\n\tCustomer Name : "+customername+"\n\tAccount Type : "+accounttype+
-		"Account Number : "+accountnumber+"\nStarting Balance : "+startbalance,"Account Information ",JOptionPane.INFORMATION_MESSAGE);
+
+	public void print() {
+		JOptionPane.showMessageDialog(null,
+				"PINCODE : " + pincode + "\n\tCustomer Name : " + customername + "\n\tAccount Type : " + accounttype +
+						"Account Number : " + accountnumber + "\nStarting Balance : " + startbalance,
+				"Account Information ", JOptionPane.INFORMATION_MESSAGE);
 	}
-   
+
 }
