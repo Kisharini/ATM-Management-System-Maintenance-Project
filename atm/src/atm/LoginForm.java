c550328aea19a852a51e7f8d86a147b8f6beab57
Kisharini Jeeva Nathan <224618@student.upm.edu.my>
2026-02-06 13:52:12 +0000

    Kisha's commit request - add serialVersionUID to LoginForm class

--- atm/src/atm/LoginForm.java
+++ atm/src/atm/LoginForm.java
@@ -7,7 +7,7 @@ import java.awt.*;
 
 public class LoginForm extends JFrame implements ActionListener
 {
-    
+    private static final long serialVersionUID = 1L;
 
     JPanel panel;
     JLabel lab1,lab2,picLab,userLab,adminLab;
